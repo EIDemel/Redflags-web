bd5b3537935cde9bfc16b9431796cbef6bb3f6f2
Leo Demeilliers <leo.demeilliers@wivetixservices.fr>
2026-07-16 10:59:51 +0200

    Support preproduction and production subpaths

--- src/main.tsx
+++ src/main.tsx
@@ -9,7 +9,7 @@ import { LanguageProvider } from "./i18n";
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <LanguageProvider>
-      <BrowserRouter>
+      <BrowserRouter basename={import.meta.env.BASE_URL}>
         <App />
       </BrowserRouter>
     </LanguageProvider>
